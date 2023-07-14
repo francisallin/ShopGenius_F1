d25bb8a739edc82450fb3f2fccaf36f174287b1c
francisallin <lamtszto1997@gmail.com>
2023-07-14 15:57:00 -0400

    update model and controller of products

--- server/models/product.js
+++ server/models/product.js
@@ -2,12 +2,14 @@ let mongoose = require('mongoose')
 
 // create a model class
 let listProduct = mongoose.Schema({
-    brand: String,
-    model: String,
-    year: Number,
-    trim: String,
-    color: String,
-    price: Number
+    firstName: String,
+    lastName: String,
+    talent: String,
+    description: String,
+    service: String,
+    price: Number,
+    remarks : String,
+    imageName : String
 },
 {
     collection: "products"
